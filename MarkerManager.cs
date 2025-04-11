4bff48aa4bef991533fa645b7f7cf6f146c1282b
Enilgles <aanert97@gmail.com>
2025-04-12 00:54:29 +1000

    Add files via upload

--- MarkerManager.cs
+++ MarkerManager.cs
@@ -1,41 +1,35 @@
-﻿using ConVar;
-using Oxide.Core;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Oxide.Core;
 using UnityEngine;
 using VLB;
 using Color = UnityEngine.Color;
 
 namespace Oxide.Plugins
 {
-    [Info("Marker Manager", "DezLife", "3.0.3")]
+    [Info("Marker Manager", "Orange", "2.1.1")]
+    [Description("Allows to place markers on in-game map")]
     public class MarkerManager : RustPlugin
     {
         #region Vars
-        public static StringBuilder StringBuilderInstance;
 
         private const string genericPrefab = "assets/prefabs/tools/map/genericradiusmarker.prefab";
         private const string vendingPrefab = "assets/prefabs/deployable/vendingmachine/vending_mapmarker.prefab";
         private const string permUse = "markermanager.use";
         private const string chatCommand = "marker";
-        private readonly List<CustomMapMarker> mapMarkers = new List<CustomMapMarker>();
+
         #endregion
 
         #region Oxide Hooks
+
         private void Init()
         {
             permission.RegisterPermission(permUse, this);
             cmd.AddChatCommand(chatCommand, this, nameof(cmdMarkerChat));
         }
-        private void OnPlayerConnected(BasePlayer player)
-        {
-            foreach (CustomMapMarker marker in mapMarkers)
-                if (marker != null) marker.UpdateMarkers();
-        }
+
         private void OnServerInitialized()
         {
-            StringBuilderInstance = new StringBuilder();
             LoadData();
             LoadCustomMarkers();
         }
@@ -44,113 +38,12 @@ namespace Oxide.Plugins
         {
             SaveData();
             RemoveMarkers();
-            StringBuilderInstance = null;
         }
 
         #endregion
 
-        #region Metods
+        #region Commands
 
-        private void CreateMarker(Vector3 position, int duration, float refreshRate, string name, string displayName, float radius = 0.3f, float alpha = 0.75f, string colorMarker = "00FFFF", string colorOutline = "00FFFFFF")
-        {
-            CustomMapMarker marker = new GameObject().AddComponent<CustomMapMarker>();
-            marker.name = name;
-            marker.displayName = displayName;
-            marker.radius = radius;
-            marker.alpha = alpha;
-            marker.position = position;
-            marker.duration = duration;
-            marker.refreshRate = refreshRate;
-            ColorUtility.TryParseHtmlString($"#{colorMarker}", out marker.color1);
-            ColorUtility.TryParseHtmlString($"#{colorOutline}", out marker.color2);
-            mapMarkers.Add(marker);
-        }
-
-        private void CreateMarker(BaseEntity entity, int duration, float refreshRate, string name, string displayName, float radius = 0.3f, float alpha = 0.75f, string colorMarker = "00FFFF", string colorOutline = "00FFFFFF")
-        {
-            CustomMapMarker marker = entity.gameObject.GetOrAddComponent<CustomMapMarker>();
-            marker.name = name;
-            marker.displayName = displayName;
-            marker.radius = radius;
-            marker.alpha = alpha;
-            marker.refreshRate = refreshRate;
-            marker.parent = entity;
-            marker.position = entity.transform.position;
-            marker.duration = duration;
-            ColorUtility.TryParseHtmlString($"#{colorMarker}", out marker.color1);
-            ColorUtility.TryParseHtmlString($"#{colorOutline}", out marker.color2);
-            mapMarkers.Add(marker);
-        }
-
-        private void RemoveMarker(string name)
-        {
-            foreach (CustomMapMarker marker in mapMarkers)
-            {
-                if (marker.name != null && marker.name == name) UnityEngine.Object.Destroy(marker);
-            }
-        }
-
-        private void RemoveMarkers()
-        {
-            foreach (CustomMapMarker marker in mapMarkers)
-            {
-                if (marker.name != null) UnityEngine.Object.Destroy(marker);
-            }
-        }
-        private void RemoveCustomMarker(string name, BasePlayer player = null)
-        {
-            int i = 0;
-            foreach (CustomMapMarker marker in mapMarkers)
-            {
-                if (marker.name == null || marker.placedByPlayer == false) continue;
-
-                if (marker.name == name)
-                {
-                    UnityEngine.Object.Destroy(marker);
-                    i++;
-                }
-            }
-            RemoveSavedMarker(name);
-            Message(player, "Removed", i);
-        }
-
-        private void CreateCustomMarker(CachedMarker def, BasePlayer player = null)
-        {
-            CustomMapMarker marker = new GameObject().AddComponent<CustomMapMarker>();
-            marker.name = def.name;
-            marker.displayName = def.displayName;
-            marker.radius = def.radius;
-            marker.alpha = def.alpha;
-            marker.position = def.position;
-            marker.duration = def.duration;
-            marker.refreshRate = def.refreshRate;
-            marker.placedByPlayer = true;
-            ColorUtility.TryParseHtmlString($"#{def.color1}", out marker.color1);
-            ColorUtility.TryParseHtmlString($"#{def.color2}", out marker.color2);
-            mapMarkers.Add(marker);
-            if(player != null) Message(player, "Added", marker.displayName, marker.position);
-        }
-
-        private void SaveCustomMarker(CachedMarker def)
-        {
-            data.Add(def);
-        }
-
-        private void LoadCustomMarkers()
-        {
-            foreach (CachedMarker def in data)
-            {
-                CreateCustomMarker(def);
-            }
-        }
-
-        private void RemoveSavedMarker(string name)
-        {
-            data.RemoveAll(x => x.name == name);
-        }
-        #endregion
-
-        #region Command
         private void cmdMarkerChat(BasePlayer player, string command, string[] args)
         {
             if (permission.UserHasPermission(player.UserIDString, permUse) == false)
@@ -179,7 +72,7 @@ namespace Oxide.Plugins
                     }
                     else
                     {
-                        CachedMarker def = new CachedMarker
+                        var def = new CachedMarker
                         {
                             position = player.transform.position,
                             name = args[1],
@@ -189,7 +82,6 @@ namespace Oxide.Plugins
                             displayName = args[5],
                             color1 = args[6],
                             color2 = args[7],
-                            alpha = Convert.ToSingle(args[8]),
                         };
 
                         CreateCustomMarker(def, player);
@@ -215,6 +107,147 @@ namespace Oxide.Plugins
 
         #endregion
 
+        #region Core
+
+        private void CreateMarker(Vector3 position, int duration, float refreshRate, string name, string displayName,
+            float radius = 0.3f, string colorMarker = "00FFFF", string colorOutline = "00FFFFFF")
+        {
+            var marker = new GameObject().AddComponent<CustomMapMarker>();
+            marker.name = name;
+            marker.displayName = displayName;
+            marker.radius = radius;
+            marker.position = position;
+            marker.duration = duration;
+            marker.refreshRate = refreshRate;
+            ColorUtility.TryParseHtmlString($"#{colorMarker}", out marker.color1);
+            ColorUtility.TryParseHtmlString($"#{colorOutline}", out marker.color2);
+        }
+
+        private void CreateMarker(BaseEntity entity, int duration, float refreshRate, string name, string displayName,
+            float radius = 0.3f, string colorMarker = "00FFFF", string colorOutline = "00FFFFFF")
+        {
+            var marker = entity.gameObject.GetOrAddComponent<CustomMapMarker>();
+            marker.name = name;
+            marker.displayName = displayName;
+            marker.radius = radius;
+            marker.refreshRate = refreshRate;
+            marker.parent = entity;
+            marker.position = entity.transform.position;
+            marker.duration = duration;
+            ColorUtility.TryParseHtmlString($"#{colorMarker}", out marker.color1);
+            ColorUtility.TryParseHtmlString($"#{colorOutline}", out marker.color2);
+        }
+
+        private void RemoveMarker(string name)
+        {
+            foreach (var marker in UnityEngine.Object.FindObjectsOfType<CustomMapMarker>())
+            {
+                if (marker.name == name)
+                {
+                    UnityEngine.Object.Destroy(marker);
+                }
+            }
+        }
+
+        private void RemoveMarkers()
+        {
+            foreach (var marker in UnityEngine.Object.FindObjectsOfType<CustomMapMarker>())
+            {
+                UnityEngine.Object.Destroy(marker);
+            }
+        }
+
+        private void CreateCustomMarker(CachedMarker def, BasePlayer player = null)
+        {
+            var marker = new GameObject().AddComponent<CustomMapMarker>();
+            marker.name = def.name;
+            marker.displayName = def.displayName;
+            marker.radius = def.radius;
+            marker.position = def.position;
+            marker.duration = def.duration;
+            marker.refreshRate = def.refreshRate;
+            marker.placedByPlayer = true;
+            ColorUtility.TryParseHtmlString($"#{def.color1}", out marker.color1);
+            ColorUtility.TryParseHtmlString($"#{def.color2}", out marker.color2);
+            Message(player, "Added", marker.displayName, marker.position);
+        }
+
+        private void RemoveCustomMarker(string name, BasePlayer player = null)
+        {
+            var i = 0;
+            foreach (var marker in UnityEngine.Object.FindObjectsOfType<CustomMapMarker>())
+            {
+                if (marker.placedByPlayer == false)
+                {
+                    continue;
+                }
+
+                if (marker.name == name)
+                {
+                    UnityEngine.Object.Destroy(marker);
+                    i++;
+                }
+            }
+
+            RemoveSavedMarker(name);
+            Message(player, "Removed", i);
+        }
+
+        private void SaveCustomMarker(CachedMarker def)
+        {
+            data.Add(def);
+        }
+
+        private void LoadCustomMarkers()
+        {
+            foreach (var def in data)
+            {
+                CreateCustomMarker(def);
+            }
+        }
+
+        private void RemoveSavedMarker(string name)
+        {
+            data.RemoveAll(x => x.name == name);
+        }
+
+        #endregion
+
+        #region Localization 1.1.1
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                {
+                    "Usage", "<color=#00ffff>Usage:</color>\n" +
+                             " <color=#00ffff>/marker add</color> name(code name) duration(seconds, 0 to permanent) refreshRate(30) radius(0.4) displayName (on map) colorInline (HEX) colorOutline (HEX) - Add marker on map\n" +
+                             " <color=#00ffff>/marker remove</color> name (code name, only for custom markers) - Remove marker from map"
+                },
+                {"Permission", "You don't have permission to use that!"},
+                {"Added", "Marker '{0}' was added on {1}!"},
+                {"Removed", "{0} markers with that name was removed!"}
+            }, this);
+        }
+
+        private string GetMessage(string messageKey, string playerID, params object[] args)
+        {
+            return string.Format(lang.GetMessage(messageKey, this, playerID), args);
+        }
+
+        private void Message(BasePlayer player, string messageKey, params object[] args)
+        {
+            if (player == null)
+            {
+                return;
+            }
+
+            var message = GetMessage(messageKey, player.UserIDString, args);
+            player.SendConsoleCommand("chat.add", (object) 0, (object) message);
+        }
+
+        #endregion
+
         #region Data 1.0.0
 
         private const string filename = "MarkerManager/Custom";
@@ -223,7 +256,6 @@ namespace Oxide.Plugins
         private class CachedMarker
         {
             public float radius;
-            public float alpha;
             public string color1;
             public string color2;
             public string displayName;
@@ -257,14 +289,18 @@ namespace Oxide.Plugins
 
         #region API
 
-        private void API_CreateMarker(Vector3 position, string name, int duration = 0, float refreshRate = 3f, float radius = 0.4f, string displayName = "Marker", string colorMarker = "00FFFF", string colorOutline = "00FFFFFF" , float alpha = 0.75f)
+        private void API_CreateMarker(Vector3 position, string name,
+            int duration = 0, float refreshRate = 3f, float radius = 0.4f,
+            string displayName = "Marker", string colorMarker = "00FFFF", string colorOutline = "00FFFFFF")
         {
-            CreateMarker(position, duration, refreshRate, name, displayName, radius, alpha, colorMarker, colorOutline);
+            CreateMarker(position, duration, refreshRate, name, displayName, radius, colorMarker, colorOutline);
         }
 
-        private void API_CreateMarker(BaseEntity entity, string name, int duration = 0, float refreshRate = 3f, float radius = 0.4f, string displayName = "Marker", string colorMarker = "00FFFF", string colorOutline = "00FFFFFF", float alpha = 0.75f)
+        private void API_CreateMarker(BaseEntity entity, string name,
+            int duration = 0, float refreshRate = 3f, float radius = 0.4f,
+            string displayName = "Marker", string colorMarker = "00FFFF", string colorOutline = "00FFFFFF")
         {
-            CreateMarker(entity, duration, refreshRate, name, displayName, radius, alpha, colorMarker, colorOutline);
+            CreateMarker(entity, duration, refreshRate, name, displayName, radius, colorMarker, colorOutline);
         }
 
         private void API_RemoveMarker(string name)
@@ -274,49 +310,8 @@ namespace Oxide.Plugins
 
         #endregion
 
-        #region Localization 1.1.1
-
-        protected override void LoadDefaultMessages()
-        {
-            lang.RegisterMessages(new Dictionary<string, string>
-            {
-                { "Usage", "<color=#00ffff>Используйте:</color>\n" + " <color=#00ffff>/marker add</color> name(code name) duration(секунды, 0 - неудолямый) refreshRate(30) radius(0.4) displayName (Названия маркера на карте) colorInline (HEX) colorOutline (HEX) alpha (прозрачность 0.75) - Добавить маркер на карту\n" + " <color=#00ffff>/marker remove</color> name (code name, только для маркеров созданных командой) - Удалить маркер с карты" },
-                {"Permission", "У вас недостаточно разрешений для использования этой команды!"},
-                {"Added", "Маркер '{0}' был добавлен {1}!"},
-                {"Removed", "{0} маркер(а) с таким названием были удалены!"},
-                {"RemovedOldVersion", "Вам нужно удалить плагин MarkerManager. Данные плагины не совместимы!"}
-            }, this, "ru");
-
-            lang.RegisterMessages(new Dictionary<string, string>
-            {
-                { "Usage", "<color=#00ffff>Usage:</color>\n" + " <color=#00ffff>/marker add</color> name(code name) duration(seconds, 0 to permanent) refreshRate(30) radius(0.4) displayName (on map) colorInline (HEX) colorOutline (HEX) alpha (0.75)  - Add marker on map\n" + " <color=#00ffff>/marker remove</color> name (code name, only for custom markers) - Remove marker from map" },
-                {"Permission", "You don't have permission to use that!"},
-                {"Added", "Marker '{0}' was added on {1}!"},
-                {"Removed", "{0} markers with that name was removed!"},
-                {"RemovedOldVersion", "You need to remove the MarkerManager plugin. These plugins are not compatible!"}
-
-            }, this);
-        }
-        public string GetLang(string LangKey, string userID = null, params object[] args)
-        {
-            StringBuilderInstance.Clear();
-            if (args != null)
-            {
-                StringBuilderInstance.AppendFormat(lang.GetMessage(LangKey, this, userID), args);
-                return StringBuilderInstance.ToString();
-            }
-            return lang.GetMessage(LangKey, this, userID);
-        }
-
-        private void Message(BasePlayer player, string messageKey, params object[] args)
-        {
-            string message = GetLang(messageKey, player.UserIDString, args);
-            player.SendConsoleCommand("chat.add", Chat.ChatChannel.Global, 0, message);
-        }
-
-        #endregion
-
         #region Scripts
+
         private class CustomMapMarker : MonoBehaviour
         {
             private VendingMachineMapMarker vending;
@@ -324,9 +319,7 @@ namespace Oxide.Plugins
             public BaseEntity parent;
             private bool asChild;
 
-            public new string name;
             public float radius;
-            public float alpha;
             public Color color1;
             public Color color2;
             public string displayName;
@@ -334,7 +327,6 @@ namespace Oxide.Plugins
             public Vector3 position;
             public int duration;
             public bool placedByPlayer;
-
 
             private void Start()
             {
@@ -345,7 +337,8 @@ namespace Oxide.Plugins
 
             private void CreateMarkers()
             {
-                vending = GameManager.server.CreateEntity(vendingPrefab, position) .GetComponent<VendingMachineMapMarker>();
+                vending = GameManager.server.CreateEntity(vendingPrefab, position)
+                    .GetComponent<VendingMachineMapMarker>();
                 vending.markerShopName = displayName;
                 vending.enableSaving = false;
                 vending.Spawn();
@@ -354,7 +347,7 @@ namespace Oxide.Plugins
                 generic.color1 = color1;
                 generic.color2 = color2;
                 generic.radius = radius;
-                generic.alpha = alpha;
+                generic.alpha = 1f;
                 generic.enableSaving = false;
                 generic.SetParent(vending);
                 generic.Spawn();
@@ -390,7 +383,7 @@ namespace Oxide.Plugins
                     }
                     else
                     {
-                        Vector3 pos = parent.transform.position;
+                        var pos = parent.transform.position;
                         transform.position = pos;
                         vending.transform.position = pos;
                     }
@@ -399,7 +392,7 @@ namespace Oxide.Plugins
                 UpdateMarkers();
             }
 
-            public void UpdateMarkers()
+            private void UpdateMarkers()
             {
                 vending.SendNetworkUpdate();
                 generic.SendUpdate();
@@ -416,6 +409,11 @@ namespace Oxide.Plugins
                 {
                     generic.Kill();
                 }
+
+                if (placedByPlayer)
+                {
+                    data.RemoveAll(x => x.name == name);
+                }
             }
 
             private void OnDestroy()
@@ -423,6 +421,7 @@ namespace Oxide.Plugins
                 DestroyMakers();
             }
         }
+
         #endregion
     }
 }
